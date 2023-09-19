7a3c816218c7297eb459f82fa556ffdad1355824
Sidiq <sidiqadinoyi15@gmail.com>
2023-09-19 09:42:02 +0100

    Contact

---
+++ script.js
@@ -0,0 +1,15 @@
+// function contact(name,email,phoneNo){
+//     this.name = name;
+//     this.email = email;
+//     this.phoneNo = phoneNo;
+// }
+
+//  contact.prototype.Info = function(){
+//     console.log(`Name: ${this.name}`);
+//     console.log(`Email: ${this.email}`);
+//     console.log(`Phone No.: ${this.phoneNo}`);
+//  };
+
+// const contact1 = new contact("Dave","dave111@gmail.com","+234-805-484-3738");
+
+// contact1.Info();
